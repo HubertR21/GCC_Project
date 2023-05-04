1cd0bb3420959e8d6e90a7df4ffae3913e354041
Szymon164 <szymonrecko1@gmail.com>
2023-05-04 18:36:50 +0200

    Adding model, index and search funcs

---
+++ DEV.py
@@ -0,0 +1,37 @@
+import clip
+import torch
+import requests
+from PIL import Image
+import numpy as np
+from io import BytesIO
+import faiss
+import pickle
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+d = 512
+index = faiss.IndexFlatIP(d)
+
+with open('filenames.txt','r') as f:
+    filenames = f.readlines()
+    f.close()
+
+for filename in filenames:
+    url = "https://storage.googleapis.com/images_gcc2023/train2017/"+filename[:-1]
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    image = preprocess(img).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        index.add(image_features)
+        if index.ntotal%100==0:
+            print(index.ntotal)
+
+
+
+file = open('index.pkl',"wb")
+pickle.dump(index,file)
+file.close()
--- main.py
+++ main.py
@@ -3,25 +3,53 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from PIL import Image
 from fastapi.staticfiles import StaticFiles
+import clip
+import torch
+import pickle
+import numpy as np
+
+storage_url = "https://storage.googleapis.com/images_gcc2023/train2017/"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+index = pickle.load(open('index.pkl',"rb"))
+with open('filenames.txt','r') as f:
+    filenames = f.readlines()
+    f.close()
+
+
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get('/', response_class=HTMLResponse)
-async def index(request: Request):
+async def clip_search(request: Request):
     context = {'request': request}
     return templates.TemplateResponse("index.html", context)
 
 
 @app.post('/', response_class=HTMLResponse)
-async def index(request: Request, image: UploadFile = File(...)):
+async def clip_search(request: Request, image: UploadFile = File(...)):
     form_data = await request.form()
-    context = {'request': request,'urls':['https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1cDnT1Q5ZrkfLfxiSgFvC2ZsjpngynJGvg&usqp=CAU']*5}
     if form_data['image'].file.read()!=bytes(0):
         pil_image = Image.open(image.file) # HERE IS THE IMAGE
-
+        image = preprocess(pil_image).unsqueeze(0).to(device)
+        with torch.no_grad():
+            image_features = model.encode_image(image)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            D, I = index.search(image_features, 20)
+            urls = list(map(lambda x: storage_url+x[:-1],np.array(filenames)[I[0]]))
+            D=np.round(D*100,2)
+        context = {'request': request,'urls':zip(urls,D[0])}
     else:
         text = form_data["text_input"] # HERE IS THE TEXT (TEXT CAN BE URL?)
-
+        with torch.no_grad():
+            text_features = model.encode_text(clip.tokenize([text]).to(device))
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            D, I = index.search(text_features, 20)
+            urls = list(map(lambda x: storage_url+x[:-1],np.array(filenames)[I[0]]))
+            D=np.round(D*100,2)
+        context = {'request': request,'urls':zip(urls,D[0])}
     return templates.TemplateResponse("images.html", context)
